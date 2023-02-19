63b07234023c46a89ae10bcefd6faf1e0b1d70dc
JoshuaAlbuquerque <22f43.joshua@sjec.ac.in>
2023-02-19 19:12:35 +0530

    collapsible buttons done

--- webpage.js
+++ webpage.js
@@ -1,25 +1,53 @@
-let col = document.getElementById("history").addEventListener("click", collapse);
+let col1 = document.getElementById("history").addEventListener("click", collapse1);
+let col2 = document.getElementById("applications").addEventListener("click", collapse2);
+let col3 = document.getElementById("benefits").addEventListener("click", collapse3);
+
 let check=false;
-function collapse()
+
+function collapse1()
 {
     if(check)
     {
-        collapseon();
+        console.log("ON");
+        document.getElementById("history").classList.toggle("opened");
         check = false;
     }
     else
     {
-        collapseoff();
+        console.log("OFF");
+        document.getElementById("history").classList.toggle("closed");
         check = true;
     }
 }
 
-function collapseon()
+function collapse2()
 {
-    document.getElementById("history").classList.toggle("opened");
+    if(check)
+    {
+        console.log("ON");
+        document.getElementById("applications").classList.toggle("opened");
+        check = false;
+    }
+    else
+    {
+        console.log("OFF");
+        document.getElementById("applications").classList.toggle("closed");
+        check = true;
+    }
 }
 
-function collapseoff()
+function collapse3()
 {
-    document.getElementById("history").classList.toggle("closed");
+    if(check)
+    {
+        console.log("ON");
+        document.getElementById("benefits").classList.toggle("opened");
+        check = false;
+    }
+    else
+    {
+        console.log("OFF");
+        document.getElementById("benefits").classList.toggle("closed");
+        check = true;
+    }
 }
